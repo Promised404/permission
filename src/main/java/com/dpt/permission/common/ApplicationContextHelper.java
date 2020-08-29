cfeab32fac9eed1bcf3c6d8fa77daefce7a3ced7
promised <1106449506@qq.com>
2020-08-29 19:02:42 +0800

    test444555

--- src/main/java/com/dpt/permission/common/ApplicationContextHelper.java
+++ src/main/java/com/dpt/permission/common/ApplicationContextHelper.java
@@ -40,5 +40,6 @@ public class ApplicationContextHelper implements ApplicationContextAware {
         System.out.println("接着测试啊啊啊啊啊啊");
         System.out.println(22222);
         System.out.println(3333);
+        System.out.println(456465465);
     }
 }
